75ea8f4c64a2198d7d424d636155cdb6ac8e89ba
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-02 15:14:42 -0400

    add clipboard logic

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,13 +1,26 @@
+import { toast } from 'react-toastify';
+
 const SingleColor = ({ color, index }) => {
   const { hex, weight } = color;
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success('color copied to clipboard');
+      } catch (error) {}
+    } else {
+      toast.error('failed to copy to clipboard');
+    }
+  };
   return (
-    <a
+    <article
       className={index > 10 ? 'color color-light' : 'color'}
       style={{ background: `#${hex}` }}
+      onClick={saveToClipboard}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">#{hex}</p>
-    </a>
+    </article>
   );
 };
 export default SingleColor;
