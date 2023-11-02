0ec7985f1d729dcad40d826086207748b4788a3f
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-02 14:43:03 -0400

    add render colors

--- src/ColorList.jsx
+++ src/ColorList.jsx
@@ -1,4 +1,13 @@
-const ColorList = () => {
-  return <div>ColorList</div>;
+import { nanoid } from 'nanoid';
+import SingleColor from './SingleColor';
+
+const ColorList = ({ colors }) => {
+  return (
+    <section className="colors">
+      {colors.map((color, index) => {
+        return <SingleColor color={color} index={index} key={nanoid()} />;
+      })}
+    </section>
+  );
 };
 export default ColorList;
--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,4 +1,13 @@
-const SingleColor = () => {
-  return <div>SingleColor</div>;
+const SingleColor = ({ color, index }) => {
+  const { hex, weight } = color;
+  return (
+    <a
+      className={index > 10 ? 'color color-light' : 'color'}
+      style={{ background: `#${hex}` }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">#{hex}</p>
+    </a>
+  );
 };
 export default SingleColor;
