a882339246e0865be5098780df1c430ea8dccfd8
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-02 14:54:09 -0400

    add toastify

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import Values from 'values.js';
 import ColorList from './ColorList';
 import Form from './Form';
 import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
   const [colors, setColors] = useState(new Values('#f15025').all(10));
+  toast.error('error');
+  toast.success('error');
   return (
     <main>
       <Form />
       <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
     </main>
   );
 };
