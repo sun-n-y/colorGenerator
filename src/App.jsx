08827526ba996a558b6c3603b48d531c2e07a67c
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-02 14:32:52 -0400

    add values js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Values from 'values.js';
 import ColorList from './ColorList';
 import Form from './Form';
+import { useState } from 'react';
 
 const App = () => {
+  const [colors, setColors] = useState(new Values('#f15025').all(10));
   return (
     <main>
       <Form />
